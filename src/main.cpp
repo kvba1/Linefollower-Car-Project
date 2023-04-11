2c419a758eaf6e754d7dc3f024c46cd85f936b7f
kvba1 <kba1337@wp.pl>
2023-04-11 09:39:39 +0200

    Init commit

---
+++ src/main.cpp
@@ -0,0 +1,63 @@
+#include <Arduino.h>
+
+#define left 36
+#define middle 34
+#define right 39
+#define DC_1 27
+#define DC_2 12
+
+// setting PWM properties
+const int freq = 5000;
+const int ledChannel = 0;
+const int resolution = 8;
+
+/*int IR_sensor_value(int anal_pin){
+  int analog = analogRead(anal_pin);
+  Serial.print(analog);
+  Serial.print("\n");
+  return analog;
+}*/
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(left,INPUT);
+  pinMode(middle,INPUT);
+  pinMode(right,INPUT);
+  Serial.print("HALO!");
+
+
+  ledcAttachPin(12, 1); // assign RGB led pins to channels
+  ledcAttachPin(14, 2);
+  ledcAttachPin(27, 3);
+  ledcAttachPin(13, 4);
+  
+  // Initialize channels 
+  // channels 0-15, resolution 1-16 bits, freq limits depend on resolution
+  // ledcSetup(uint8_t channel, uint32_t freq, uint8_t resolution_bits);
+  ledcSetup(1, 12000, 8); // 12 kHz PWM, 8-bit resolution
+  ledcSetup(2, 12000, 8);
+  ledcSetup(3, 12000, 8);
+  ledcSetup(4, 12000, 8);
+}
+
+void loop() {
+int anal_lewy = analogRead(left);
+int anal_srodkowy = analogRead(middle);
+int anal_prawy = analogRead(right);
+Serial.print(anal_lewy);
+Serial.print(" - lewy\n");
+Serial.print(anal_srodkowy);
+Serial.print(" - srodkowy\n");
+Serial.print(anal_prawy);
+Serial.print(" - prawy\n");
+Serial.print("\n");
+if (anal_lewy > anal_prawy) Serial.print("skrec w prawo");
+if (anal_prawy > anal_lewy) Serial.print("skrec w lewo");
+Serial.print("\n");
+Serial.print("\n");
+ledcWrite(1, 255);
+  ledcWrite(2, 180);
+  ledcWrite(3, 0);
+  ledcWrite(4, 0);
+delay(1000);
+}
